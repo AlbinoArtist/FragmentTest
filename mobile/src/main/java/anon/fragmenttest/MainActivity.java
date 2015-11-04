1078d5bf258a40d125edfb05d7d54efb3f07c8ba
hehp123 <dic12hed@student.lu.se>
2015-11-04 21:52:51 +0100

    Initial commit

---
+++ mobile/src/main/java/anon/fragmenttest/MainActivity.java
@@ -0,0 +1,37 @@
+package anon.fragmenttest;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction sfm = getSupportFragmentManager().beginTransaction();
+                sfm.replace(R.id.placeholder,MainActivityFragment2.newInstance());
+                sfm.commit();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
---
+++ mobile/src/main/java/anon/fragmenttest/SupportFragment.java
@@ -0,0 +1,7 @@
+package anon.fragmenttest;
+
+/**
+ * Created by Henrik on 2015-11-04.
+ */
+public class SupportFragment {
+}
